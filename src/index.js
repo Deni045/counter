9b93c1e756239bca09772b9c83168e76333879c9
Deni <06denidzh@gmail.com>
2022-10-31 17:33:43 +0300

    prop

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import './index.css';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <div>
-    <p>farjngrgnreigniqregnuqgnqrnufqdjfnaijon</p>
-    <p>sdadasdsadsdlmdlas</p>
-    <span>sdasdasdsdasooooo</span>
+        <p><Burger/></p>
+        <Name name ='bobby shmurda' age ='111'/>
+        <p><Tross/></p>
    </div>
 )
 
+function Tross(){
+    return(
+        <>
+        <span className="Tross">012345678901245687890123456789012345678901234567890123456789012345678901234567890123456789</span>
+        </>
+    )
+}
 
+function Burger(){
+    return(
+        <>
+        <span className="unknown">asdfghjkoiuytresxcvbnmkloiuytgfvbnm,lkiuygfvbnm,.løp0987654</span>
+       </> 
+    )
+}
 
+function Name(props){
+return(
+     <>
+
+     <span className="span">
+         {props.name}
+     </span>
+
+     <span className="span">
+         {props.age}
+     </span>
+    
+    </> 
+    )
+}
